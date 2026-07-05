7ff8aa4775a20e91980d7ee6e35109cadd4c0b17
Yu Sasaki <uyu17018@gmail.com>
2026-07-05 16:38:44 +0900

    Simplify Render deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,11 +4,11 @@ set -e
 echo "Running composer install..."
 composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
+echo "Clearing stale caches..."
+php artisan optimize:clear
+
 echo "Caching config..."
 php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
