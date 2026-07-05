c800662115980a82e70fac0b6ad86b76046cd072
Yu Sasaki <uyu17018@gmail.com>
2026-07-05 17:01:29 +0900

    Reset production database on deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -10,8 +10,5 @@ php artisan optimize:clear
 echo "Caching config..."
 php artisan config:cache
 
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Seeding default data..."
-php artisan db:seed --force
+echo "Rebuilding production database..."
+php artisan migrate:fresh --seed --force
